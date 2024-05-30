e5f199a3d1f614b385c503b704dcec7182eb4330
yong woo Yun <yongwooyun@hotmail.com>
2024-05-29 23:30:18 -0700

    npm run seed working data inserted and api routes done

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -13,6 +13,7 @@ router.get('/', async(req, res) => {
     });
     res.status(200).json(categoryData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 
@@ -35,6 +36,7 @@ router.get('/:id', async(req, res) => {
 
     res.status(200).json(categoryData);
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
  
@@ -44,14 +46,62 @@ router.get('/:id', async(req, res) => {
 
 router.post('/', async(req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+
+  try {
+    const updatedCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!updatedCategory[0]) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+
+
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+
 });
 
 module.exports = router;
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,19 +4,52 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+
+
+
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+
+
+
+
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/',  (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -87,13 +120,28 @@ router.put('/:id', (req, res) => {
       return res.json(product);
     })
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then(productData => {
+    if (!productData) {
+      res.status(404).json({ message: 'There is no product associated with this id!' });
+      return;
+    }
+    res.status(200).json(productData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
