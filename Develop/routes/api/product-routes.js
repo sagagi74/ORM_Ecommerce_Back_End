d1cb5baaa31a275a0eb5211049cef592e4dee7da
yong woo Yun <yongwooyun@hotmail.com>
2024-05-31 03:02:00 -0700

    fixing routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -51,6 +51,7 @@ router.post('/', async (req, res) => {
   }
 });
 
+
 // UPDATE a product by ID
 router.put('/:id', async (req, res) => {
   try {
@@ -58,28 +59,7 @@ router.put('/:id', async (req, res) => {
       where: { id: req.params.id },
     });
 
-    if (req.body.tagIds && req.body.tagIds.length) {
-      const productTags = await ProductTag.findAll({ where: { product_id: req.params.id } });
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => ({
-          product_id: req.params.id,
-          tag_id,
-        }));
-
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      await Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    }
-
-    res.status(200).json({ message: 'this Product updated' });
+    res.status(200).json({ message: 'Product updated' });
   } catch (err) {
     res.status(400).json(err);
   }
